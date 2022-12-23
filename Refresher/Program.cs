f5e0582275b298b76445269f114dc956703d2198
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-12-22 16:24:00 -0800

    Re-patching support, no heap allocations, use space provided by extra padding after string in some binaries

--- Refresher/Program.cs
+++ Refresher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.IO.MemoryMappedFiles;
 using CommandLine;
 using Refresher.Patching;
 using Refresher.Verification;
@@ -30,35 +31,99 @@ public class Program
 
     private static void CommandLine(CommandlineOptions options)
     {
-        byte[] data;
-        try
-        {
-            data = File.ReadAllBytes(options.InputFile);
+        //Deletes the temparary file, if it exists
+        void DeleteTempFile(string s) {
+            try {
+                if (s != null)
+                    File.Delete(s);
+            }
+            catch { /* */ }
         }
-        catch (Exception e)
-        {
-            Console.WriteLine("Could not read data from the input file.\n" + e);
+
+        //If the input file does not exist, exit
+        if (!File.Exists(options.InputFile)) {
+            Console.WriteLine("Input file does not exist.");
             Environment.Exit(1);
             return;
         }
 
-        Patcher patcher = new(data);
+        string tempFile = null;
+        try {
+            //Create a temp file to store the EBOOT as we work on it
+            tempFile = Path.GetTempFileName();
+            
+            //Copy the input file to the temp file
+            File.Copy(options.InputFile, tempFile, true);
+        }
+        catch(Exception e) {
+            Console.WriteLine("Could not create and copy to temporary file.\n" + e);
+
+            DeleteTempFile(tempFile);
+
+            Environment.Exit(1);
+            return; 
+        }
+
+        // FileStream       tempFileStream;
+        MemoryMappedFile mappedFile;
+        try
+        {
+            //Open a write stream to the file
+            
+            // tempFileStream = File.Open(tempFile, FileMode.Open, FileAccess.ReadWrite);
+            mappedFile     = MemoryMappedFile.CreateFromFile(tempFile, FileMode.Open, null, 0, MemoryMappedFileAccess.ReadWrite);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Could not read data from the input file.\n" + e);
+            
+            DeleteTempFile(tempFile);
+            
+            Environment.Exit(1);
+            return;
+        }
+
+        //Create a new patcher with the temp file stream
+        Patcher       patcher  = new(mappedFile.CreateViewStream());
         List<Message> messages = patcher.Verify(options.ServerUrl).ToList();
         
+        //Write the messages to the console
         foreach (Message message in messages)
         {
             Console.WriteLine($"{message.Level}: {message.Content}");
         }
 
+        //If there are any errors, exit
         if (messages.Any(m => m.Level == MessageLevel.Error))
         {
             Console.WriteLine("\nThe patching operation cannot continue due to errors while verifying. Stopping.");
+            
+            mappedFile.Dispose();
+            
+            DeleteTempFile(tempFile);
+            
             Environment.Exit(1);
             return;
         }
         
-        patcher.PatchUrl(options.ServerUrl);
-        
-        File.WriteAllBytes(options.OutputFile, patcher.Data);
+        try {
+            //Patch the file
+            patcher.PatchUrl(options.ServerUrl);
+            
+            //TODO: warn the user if they are overwriting the file
+            File.Move(tempFile, options.OutputFile, true);
+        }
+        catch(Exception e) {
+            Console.WriteLine("Could not complete patch stopping.\n" + e);
+            
+            mappedFile.Dispose();
+            
+            DeleteTempFile(tempFile);
+            
+            Environment.Exit(1);
+            return; 
+        }
+
+        DeleteTempFile(tempFile);
     }
 }
