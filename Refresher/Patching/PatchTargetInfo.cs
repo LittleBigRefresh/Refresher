437519c5fff0857dbd770fdb9428491ce1c8da68
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-08-23 23:02:45 -0700

    Fix formatting on PatchTargetInfo

--- Refresher/Patching/PatchTargetInfo.cs
+++ Refresher/Patching/PatchTargetInfo.cs
@@ -1,7 +1,8 @@
 namespace Refresher.Patching;
 
-public struct PatchTargetInfo {
-    public long   Offset;
-    public int    Length;
+public struct PatchTargetInfo
+{
+    public long Offset;
+    public int Length;
     public string Data;
 }
