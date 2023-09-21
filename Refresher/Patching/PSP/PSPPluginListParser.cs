c1f6152cb13c02892b2bf53688d2cf0b8c0aa2e7
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-09-20 18:37:58 -0700

    Non-destructively update user's game.txt file

---
+++ Refresher/Patching/PSP/PSPPluginListParser.cs
@@ -0,0 +1,44 @@
+namespace Refresher.Patching.PSP;
+
+public static class PSPPluginListParser
+{
+    public static List<PSPPluginListEntry> Parse(TextReader reader)
+    {
+        List<PSPPluginListEntry> list = new();
+
+        while (reader.ReadLine() is { } line)
+        {
+            //Skip blank lines
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            
+            string[] parts = line.Split(" ");
+
+            PSPPluginListEntry entry = new(parts[0]);
+            
+            if (parts.Length > 1)
+            {
+                entry.Type = int.Parse(parts[1]);
+            }
+            
+            list.Add(entry);
+        }
+        
+        return list;
+    }
+
+    public static void Write(List<PSPPluginListEntry> list, TextWriter writer)
+    {
+        foreach (PSPPluginListEntry entry in list)
+        {
+            writer.Write(entry.Path);
+            if (entry.Type.HasValue)
+            {
+                writer.Write(' ');
+                writer.Write(entry.Type.Value);
+            }
+            writer.Write('\n');
+        }
+        
+        writer.Flush();
+    }
+}
--- Refresher/Patching/PSPPatcher.cs
+++ Refresher/Patching/PSPPatcher.cs
@@ -1,3 +1,4 @@
+using Refresher.Patching.PSP;
 using Refresher.Verification;
 
 namespace Refresher.Patching;
@@ -21,7 +22,7 @@ public class PSPPatcher : IPatcher
 
     public void Patch(string url, bool patchDigest)
     {
-        Uri uri = new Uri(url);
+        Uri uri = new(url);
 
         string domain = uri.Host;
         string format = $"{uri.Scheme}://%s:{uri.Port}{uri.AbsolutePath}%s";
@@ -34,14 +35,68 @@ public class PSPPatcher : IPatcher
             //Create it
             Directory.CreateDirectory(pluginsDir);
         }
-
+        
         string domainPath = Path.Combine(pluginsDir, "Allefresher_domain.txt");
         string formatPath = Path.Combine(pluginsDir, "Allefresher_format.txt");
         
+        //Delete the existing domain and format configuration files
         File.Delete(domainPath);
         File.Delete(formatPath);
         
+        //Write the new domain and format configuration
         File.WriteAllText(domainPath, domain);
         File.WriteAllText(formatPath, format);
+
+        //Match for all files called "game.txt" in the plugins directory
+        //NOTE: we do this because the PSP filesystem is case insensitive, and the .NET STL is case sensitive on linux
+        List<string> possibleMatches = Directory.EnumerateFiles(pluginsDir, "game.txt", new EnumerationOptions
+        {
+            MatchCasing = MatchCasing.CaseInsensitive,
+        }).ToList();
+
+        FileStream gamePluginsFileStream;
+
+        const string allefresherPath = "ms0:/SEPLUGINS/Allefresher.prx";
+        
+        List<PSPPluginListEntry> entries;
+        if (possibleMatches.Any())
+        {
+            //Open the first match
+            FileStream stream = File.OpenRead(possibleMatches[0]);
+
+            //Read out the matches
+            entries = PSPPluginListParser.Parse(new StreamReader(stream));
+
+            //If Allefresher is not in the list,
+            if (!entries.Any(entry => entry.Path.Contains("Allefresher.prx")))
+            {
+                //Add Allefresher to the game plugin list
+                entries.Add(new PSPPluginListEntry(allefresherPath, 1));
+            }
+
+            //Dispose the read stream
+            stream.Dispose();
+
+            //Open a new write stream to the game.txt file
+            gamePluginsFileStream = File.Open(possibleMatches[0], FileMode.Truncate);
+        }
+        else
+        {
+            //Create a new list, with the only entry being Allefresher
+            entries = new List<PSPPluginListEntry>
+            {
+                new(allefresherPath, 1),
+            };
+
+            //Create a new game.txt file
+            gamePluginsFileStream = File.Open(Path.Combine(pluginsDir, "game.txt"), FileMode.CreateNew);
+        }
+        
+        //Write the plugin list to the file
+        PSPPluginListParser.Write(entries, new StreamWriter(gamePluginsFileStream));
+
+        //Flush and dispose the stream
+        gamePluginsFileStream.Flush();
+        gamePluginsFileStream.Dispose();
     }
 }
