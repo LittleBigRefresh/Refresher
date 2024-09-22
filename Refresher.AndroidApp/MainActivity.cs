cc950d3c3865dd7060173f73a90095d05239b0b4
jvyden <jvyden@jvyden.xyz>
2024-09-22 03:40:09 -0400

    Pipeline listing and activity

--- Refresher.AndroidApp/MainActivity.cs
+++ Refresher.AndroidApp/MainActivity.cs
@@ -1,3 +1,7 @@
+using _Microsoft.Android.Resource.Designer;
+using Android.Content;
+using Refresher.Core.Pipelines;
+
 namespace Refresher.AndroidApp;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
@@ -8,6 +12,30 @@ public class MainActivity : Activity
         base.OnCreate(savedInstanceState);
 
         // Set our view from the "main" layout resource
-        SetContentView(Resource.Layout.activity_main);
+        this.SetContentView(ResourceConstant.Layout.activity_main);
+
+        LinearLayout? mainContent = this.FindViewById<LinearLayout>(Resource.Id.MainContent);
+        if (mainContent == null)
+            throw new Exception("Main content not found");
+
+        this.AddButtonForPipeline<ExamplePipeline>(mainContent);
+    }
+
+    private void AddButtonForPipeline<TPipeline>(LinearLayout layout) where TPipeline : Pipeline
+    {
+        Button button = new(this);
+        button.Text = typeof(TPipeline).Name;
+
+        button.Click += (_, _) =>
+        {
+            Toast.MakeText(this, button.Text, ToastLength.Short)?.Show();
+            Intent intent = new(this, typeof(PipelineActivity));
+            
+            intent.PutExtra("PipelineType", typeof(TPipeline).FullName);
+
+            this.StartActivity(intent);
+        };
+
+        layout.AddView(button);
     }
 }
---
+++ Refresher.AndroidApp/PipelineActivity.cs
@@ -0,0 +1,28 @@
+using _Microsoft.Android.Resource.Designer;
+using Refresher.Core.Pipelines;
+
+namespace Refresher.AndroidApp;
+
+[Activity]
+public class PipelineActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        this.SetContentView(ResourceConstant.Layout.activity_pipeline);
+        
+        string? pipelineTypeName = this.Intent?.GetStringExtra("PipelineType");
+        if(pipelineTypeName == null)
+            throw new Exception("Pipeline type not specified");
+
+        Type? pipelineType = typeof(Pipeline).Assembly.GetType(pipelineTypeName);
+        if(pipelineType == null)
+            throw new Exception("Pipeline was not found");
+
+        Pipeline pipeline = (Pipeline)Activator.CreateInstance(pipelineType)!;
+        
+        TextView textView = new(this);
+        textView.Text = $"{pipeline.Name}";
+        this.SetContentView(textView);
+    }
+}
