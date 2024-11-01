328d44818b26366008dcb2ece201f3675067ed61
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-11-01 14:19:27 -0700

    A missing content ID is not a critical error

--- Refresher.Core/Pipelines/Steps/ReadEbootContentIdStep.cs
+++ Refresher.Core/Pipelines/Steps/ReadEbootContentIdStep.cs
@@ -16,12 +16,14 @@ public class ReadEbootContentIdStep : Step
         this.Progress = 0.5f;
         
         string? contentId = LibSceToolSharp.GetContentId(ebootPath)?.TrimEnd('\0');
-        if (contentId == null)
-            throw new Exception("Unable to retrieve the content ID from the game's EBOOT.");
+        this.Game.ContentId = contentId;
+
         this.Progress = 1f;
 
-        this.Game.ContentId = contentId;
-        State.Logger.LogDebug(InfoRetrieval, "Got content ID from the game's EBOOT: {0}", contentId);
+        if(contentId != null)
+            State.Logger.LogDebug(InfoRetrieval, "Got content ID from the game's EBOOT: {0}", contentId);
+        else
+            State.Logger.LogDebug(InfoRetrieval, "Unable to find content ID in the game's EBOOT.");
         return Task.CompletedTask;
     }
 }
