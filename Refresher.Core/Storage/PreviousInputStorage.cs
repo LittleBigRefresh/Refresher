6ea3e85cb2dd77b464d3f1aa6ebbc1e845833cbc
jvyden <jvyden@jvyden.xyz>
2025-05-26 14:17:37 -0400

    Add mechanism to cache previously used inputs

---
+++ Refresher.Core/Storage/PreviousInputStorage.cs
@@ -0,0 +1,70 @@
+﻿using Refresher.Core.Pipelines;
+
+namespace Refresher.Core.Storage;
+
+/// <summary>
+/// Stores used inputs in pipelines for use in later sessions of Refresher.
+/// </summary>
+public static class PreviousInputStorage
+{
+    private const string ApplicationDirectory = "Refresher";
+    private const string FileName = "inputs.ini";
+    private static readonly string FilePath;
+    
+    public static readonly Dictionary<string, string> StoredInputs = [];
+    
+    static PreviousInputStorage()
+    {
+        string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        FilePath = Path.Combine(appData, ApplicationDirectory, FileName);
+    }
+
+    public static void Read()
+    {
+        if (!File.Exists(FilePath))
+            return;
+        
+        using FileStream stream = File.OpenRead(FilePath);
+        using StreamReader reader = new(stream);
+
+        while (!reader.EndOfStream)
+        {
+            string? line = reader.ReadLine();
+            if (line == null)
+                break;
+            
+            if(string.IsNullOrWhiteSpace(line) || line.StartsWith(';') || !line.Contains('='))
+                continue;
+
+            int equals = line.IndexOf('=');
+
+            string key = line[..equals];
+            string value = line[(equals + 1)..];
+
+            StoredInputs[key] = value;
+        }
+    }
+
+    public static void ApplyFromPipeline(Pipeline pipeline)
+    {
+        foreach ((string key, string value) in pipeline.Inputs)
+        {
+            StoredInputs[key] = value;
+        }
+    }
+
+    public static void Write()
+    {
+        using FileStream stream = File.OpenWrite(FilePath);
+        using StreamWriter writer = new(stream);
+        foreach ((string key, string value) in StoredInputs)
+        {
+            writer.Write(key);
+            writer.Write('=');
+            writer.WriteLine(value);
+        }
+        
+        writer.Flush();
+        stream.Flush();
+    }
+}
--- Refresher/UI/PipelineForm.cs
+++ Refresher/UI/PipelineForm.cs
@@ -118,29 +118,32 @@ public class PipelineForm<TPipeline> : RefresherForm where TPipeline : Pipeline,
         this._controller = new PipelineController(this._pipeline, Application.Instance.Invoke);
         this._pipeline.Initialize();
         
+        PreviousInputStorage.Read();
         this._formLayout.Rows.Clear();
         foreach (StepInput input in this._pipeline.RequiredInputs)
         {
+            PreviousInputStorage.StoredInputs.TryGetValue(input.Id, out string? value);
+            
             TableRow row;
             switch (input.Type)
             {
                 case StepInputType.Game:
-                    row = AddField<DropDown>(input);
+                    row = AddField<DropDown>(input, value);
                     this._gamesDropDown = row.Cells[1].Control as DropDown;
                     this._gamesDropDown!.Enabled = false;
                     this._gamesDropDown.Height = 56;
                     break;
                 case StepInputType.Url:
                 case StepInputType.Text:
-                    row = AddField<TextBox>(input);
+                    row = AddField<TextBox>(input, value);
                     break;
                 case StepInputType.Directory:
-                    row = AddField<FilePicker>(input);
+                    row = AddField<FilePicker>(input, value);
                     if (input.ShouldCauseGameDownloadWhenChanged)
                         (row.Cells[1].Control as FilePicker)!.FilePathChanged += this.OnDownloadGameList;
                     break;
                 case StepInputType.ConsoleIp:
-                    row = AddField<TextBox>(input, this._connectButton = new Button(this.OnDownloadGameList) { Text = "Connect" });
+                    row = AddField<TextBox>(input, value, this._connectButton = new Button(this.OnDownloadGameList) { Text = "Connect" });
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
@@ -415,7 +418,7 @@ public class PipelineForm<TPipeline> : RefresherForm where TPipeline : Pipeline,
         MessageBox.Show("The log was successfully copied to the clipboard.", "Success");
     }
     
-    private static TableRow AddField<TControl>(StepInput input, Button? button = null) where TControl : Control, new()
+    private static TableRow AddField<TControl>(StepInput input, string? value = null, Button? button = null) where TControl : Control, new()
     {
         Label label = new()
         {
@@ -428,6 +431,7 @@ public class PipelineForm<TPipeline> : RefresherForm where TPipeline : Pipeline,
         TextBox? textBox = control as TextBox;
 
         string? newValue = input.DetermineDefaultValue?.Invoke();
+        newValue ??= value;
 
         if (textBox != null)
         {
