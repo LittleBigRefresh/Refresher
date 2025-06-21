98d2b33c58eec887451a318a0f01034e45a5c1aa
jvyden <jvyden@jvyden.xyz>
2025-06-21 15:54:57 -0400

    Warn about using Windows search links being used as emulator path

--- Refresher.Core/Pipelines/Steps/SetupEmulatorAccessorStep.cs
+++ Refresher.Core/Pipelines/Steps/SetupEmulatorAccessorStep.cs
@@ -16,6 +16,14 @@ public class SetupEmulatorAccessorStep : Step
     public override Task ExecuteAsync(CancellationToken cancellationToken = default)
     {
         string path = this.Inputs[0].GetValueFromPipeline(this.Pipeline);
+
+        // https://littlebigrefresh.sentry.io/issues/6636592943
+        if (path.StartsWith("search-ms:"))
+            return this.Fail(
+                "The path you entered is a Windows search link. " +
+                "Please use the real file path. " +
+                "(it usually starts with C:\\ or some other drive letter)");
+        
         State.Logger.LogDebug(RPCS3, $"Using RPCS3 path {path}");
         this.Pipeline.Accessor = new EmulatorPatchAccessor(path);
 
